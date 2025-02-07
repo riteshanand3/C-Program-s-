69b3b3e06f396554505520585ad3cf64f91b1751
riteshanand3 <i.riteshanand@gmail.com>
2025-02-07 17:27:02 +0530

    new program

---
+++ Sumof2no.c
@@ -0,0 +1,17 @@
+// Sum of 2 Numbers
+
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("sum is : %d", sum);
+
+    return 0;
+    
+}
---
+++ arofsquare.c
@@ -0,0 +1,11 @@
+// Write a program to calculate area of sqaure  (side given)
+
+#include<stdio.h>
+int main() {
+    int side;
+    printf("enter side");
+    scanf("%d", &side);
+
+        printf("area is : %d", side * side);
+        return 0;
+}
